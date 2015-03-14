5bb5a92080731ab275f4396bff307c15729d4c33
Chris Sprague <chris@chaoscontrol.org>
2015-03-14 13:26:59 -0400

    init word-count puzzle

---
+++ objective-c/word-count/word-count/word-count/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  word-count
+//
+//  Created by Chris Sprague on 3/14/15.
+//  Copyright (c) 2015 exercism. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
